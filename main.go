3a91b28708b9519ca27a6506f73ded527bdca8e2
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-01-31 08:40:38 +0000

    Make YAML look pretty on standard output

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"io/ioutil"
 	"os"
 
+	"github.com/d4l3k/go-highlight"
+	"github.com/docker/docker/pkg/term"
 	"github.com/spf13/cobra"
 
 	"github.com/errordeveloper/kubegen/appmaker"
@@ -18,34 +20,46 @@ var (
 
 func main() {
 	var rootCmd = &cobra.Command{
-		Use:  "kubegen",
-		RunE: kubegen,
+		Use: "kubegen",
 	}
 
-	rootCmd.Flags().StringVarP(&manifest, "manifest", "", "", "App manifest to use")
-	rootCmd.Flags().StringVarP(&image, "image", "", "", "Image to use")
-	rootCmd.Flags().StringVarP(&flavor, "flavor", "", "default", "Flavor of generator to use")
+	//var component = &cobra.Command{
+	//	Use:  "kubegen ",
+	//	RunE: generateComponent,
+	//}
+
+	var appStack = &cobra.Command{
+		Use:  "stack",
+		RunE: generateAppStack,
+	}
+
+	appStack.Flags().StringVarP(&manifest, "manifest", "", "Kubefile", "App manifest to use")
+	rootCmd.AddCommand(appStack)
+
+	//component.Flags().StringVarP(&image, "image", "", "", "Image to use")
+	//component.Flags().StringVarP(&flavor, "flavor", "", "default", "Flavor of generator to use")
+	//rootCmd.AddCommand(component)
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
 
-func kubegen(cmd *cobra.Command, args []string) error {
-	if manifest != "" {
-		data, err := ioutil.ReadFile(manifest)
-		if err != nil {
-			return err
-		}
-
-		app, err := appmaker.NewFromHCL(data)
-		if err != nil {
-			return err
-		}
-
-		return encodeAndOutput(app)
+func generateAppStack(cmd *cobra.Command, args []string) error {
+	data, err := ioutil.ReadFile(manifest)
+	if err != nil {
+		return err
 	}
 
+	app, err := appmaker.NewFromHCL(data)
+	if err != nil {
+		return err
+	}
+
+	return encodeAndOutput(app)
+}
+
+/*
 	if image == "" {
 		return fmt.Errorf("Image must be specified")
 	}
@@ -61,13 +75,25 @@ func kubegen(cmd *cobra.Command, args []string) error {
 	}
 
 	return encodeAndOutput(app)
-}
+*/
 
 func encodeAndOutput(app *appmaker.App) error {
 	data, err := app.EncodeListToYAML()
 	if err != nil {
 		return err
 	}
-	fmt.Println(string(data))
+
+	var output string
+	if term.IsTerminal(0) {
+		pretty, err := highlight.Term("yaml", data)
+		if err != nil {
+			panic(err)
+		}
+		output = string(pretty)
+	} else {
+		output = string(data)
+	}
+	fmt.Println(output)
+
 	return nil
 }
