88d1dda1440fe0261e0098379b98fb7f497173c9
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-07-28 14:11:20 +0100

    Minor cleanup

--- pkg/modules/walker.go
+++ pkg/modules/walker.go
@@ -21,14 +21,13 @@ type interpolationWalker struct {
 	// context-aware validation.
 	ContextF interpolationWalkerContextFunc
 
-	key         []string
-	lastValue   reflect.Value
-	loc         reflectwalk.Location
-	cs          []reflect.Value
-	csKey       []reflect.Value
-	csData      interface{}
-	sliceIndex  []int
-	unknownKeys []string
+	key        []string
+	lastValue  reflect.Value
+	loc        reflectwalk.Location
+	cs         []reflect.Value
+	csKey      []reflect.Value
+	csData     interface{}
+	sliceIndex []int
 }
 
 type interpolationWalkerFunc func(map[string]interface{}) (map[string]interface{}, error)
@@ -87,8 +86,6 @@ func (w *interpolationWalker) Map(m reflect.Value) error {
 func (w *interpolationWalker) MapElem(m, k, v reflect.Value) error {
 	w.csData = k
 	w.csKey = append(w.csKey, k)
-	//fmt.Printf("MapElem.k: %#v\n", k)
-	//fmt.Printf("MapElem.v: %#v\n", v)
 
 	if l := len(w.sliceIndex); l > 0 {
 		w.key = append(w.key, fmt.Sprintf("%d.%s", w.sliceIndex[l-1], k.String()))
@@ -111,98 +108,6 @@ func (w *interpolationWalker) SliceElem(i int, elem reflect.Value) error {
 	return nil
 }
 
-/*
-func (w *interpolationWalker) Primitive(v reflect.Value) error {
-	setV := v
-
-	// We only care about strings
-	if v.Kind() == reflect.Interface {
-		setV = v
-		v = v.Elem()
-	}
-	if v.Kind() != reflect.String {
-		return nil
-	}
-
-	if w.ContextF != nil {
-		//w.ContextF(w.loc, astRoot)
-		w.ContextF(w.loc, v)
-	}
-
-	if w.F == nil {
-		return nil
-	}
-
-	//replaceVal, err := w.F(astRoot)
-	replaceVal, err := w.F(v)
-	if err != nil {
-		return fmt.Errorf(
-			"%s in:\n\n%s",
-			err, v.String())
-	}
-
-	switch w.loc {
-	case reflectwalk.MapValue:
-		fmt.Printf("w.(MapValue): %#v\n", w.cs[len(w.cs)-1])
-	case reflectwalk.MapKey:
-		fmt.Printf("w.(MapKey): %#v\n", w.cs[len(w.cs)-1])
-	}
-
-	if w.Replace {
-		// We need to determine if we need to remove this element
-		// if the result contains any "UnknownVariableValue" which is
-		// set if it is computed. This behavior is different if we're
-		// splitting (in a SliceElem) or not.
-		remove := false
-		if w.loc == reflectwalk.SliceElem {
-			switch typedReplaceVal := replaceVal.(type) {
-			case string:
-				if typedReplaceVal == UnknownVariableValue {
-					remove = true
-				}
-			case []interface{}:
-				if hasUnknownValue(typedReplaceVal) {
-					remove = true
-				}
-			}
-		} else if replaceVal == UnknownVariableValue {
-			remove = true
-		}
-
-		if remove {
-			w.unknownKeys = append(w.unknownKeys, strings.Join(w.key, "."))
-		}
-
-		resultVal := reflect.ValueOf(replaceVal)
-		switch w.loc {
-		case reflectwalk.MapKey:
-			m := w.cs[len(w.cs)-1]
-
-			// Delete the old value
-			var zero reflect.Value
-			m.SetMapIndex(w.csData.(reflect.Value), zero)
-
-			// Set the new key with the existing value
-			m.SetMapIndex(resultVal, w.lastValue)
-
-			// Set the key to be the new key
-			w.csData = resultVal
-		case reflectwalk.MapValue:
-			// If we're in a map, then the only way to set a map value is
-			// to set it directly.
-			m := w.cs[len(w.cs)-1]
-			mk := w.csData.(reflect.Value)
-			m.SetMapIndex(mk, resultVal)
-		default:
-			// Otherwise, we should be addressable
-			setV.Set(resultVal)
-		}
-	}
-
-	return nil
-}
-*/
-
 func (w *interpolationWalker) replaceCurrent(v reflect.Value) {
 	// if we don't have at least 2 values, we're not going to find a map, but
 	// we could panic.
@@ -218,19 +123,6 @@ func (w *interpolationWalker) replaceCurrent(v reflect.Value) {
 		c.SetMapIndex(k, v)
 	}
 }
-
-/*
-func hasUnknownValue(variable []interface{}) bool {
-	for _, value := range variable {
-		if strVal, ok := value.(string); ok {
-			if strVal == UnknownVariableValue {
-				return true
-			}
-		}
-	}
-	return false
-}
-*/
 
 func (w *interpolationWalker) splitSlice() error {
 	raw := w.cs[len(w.cs)-1]
